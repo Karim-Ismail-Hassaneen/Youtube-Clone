7b14e07254c208b79a12a5d192eeb14f1a8f0cba
Karim-Ismail-Hassaneen <121400836+Karim-Ismail-Hassaneen@users.noreply.github.com>
2023-12-07 18:18:42 +0200

    Edited

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import ThemeRegistry from '@/component/ThemeRegistry/ThemeRegistry'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './component/Navbar'
@@ -14,10 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-       <ThemeRegistry>
             <Navbar/>
         {children}
-        </ThemeRegistry>
         </body>
     </html>
   )
